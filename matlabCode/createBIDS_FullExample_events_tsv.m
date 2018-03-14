327e1941aa4e6eaa0ff5bae90ff68f30b6a9fa80
Anushka Bhandari <anushka16134@iiitd.ac.in>
2018-03-14 21:38:56 +0530

    Update and rename createBIDS_balloonanalog_events_tsv.m to createBIDS_FullExample_events_tsv.m
    updated _events.tsv code

--- matlabCode/createBIDS_FullExample_events_tsv.m
+++ matlabCode/createBIDS_FullExample_events_tsv.m
@@ -10,7 +10,7 @@ root_dir = '../';
 project_label = 'templates';
 sub_id = '01';
 ses_id = '01';
-task_id = 'balloonanalogrisktask'; %example task 'balloonanalogrisktask'
+task_id = 'FullExample'; 
 
 acquisition = 'func';
 run_id='01';
@@ -26,22 +26,22 @@ events_tsv_name = fullfile(root_dir,project_label,...
               
 %% make a _events table and save 
 %% CONTAINS a set of REQUIRED and OPTIONAL columns
-onset = [0.061]'; %REQUIRED Onset (in seconds) of the event  measured from the beginning of the acquisition of the first volume in the corresponding task imaging data file.  If any acquired scans have been discarded before forming the imaging data file, ensure that a time of 0 corresponds to the first image stored. In other words negative numbers in “onset” are allowed.
+onset = [0]'; %REQUIRED Onset (in seconds) of the event  measured from the beginning of the acquisition of the first volume in the corresponding task imaging data file.  If any acquired scans have been discarded before forming the imaging data file, ensure that a time of 0 corresponds to the first image stored. In other words negative numbers in “onset” are allowed.
 
 % REQUIRED. Duration of the event (measured  from onset) in seconds.  Must always be either zero or positive. A "duration" value of zero implies that the delta function or event is so short as to be effectively modeled as an impulse.
-duration = [0.772]'; 
+duration = [0]'; 
 
 %OPTIONAL Primary categorisation of each trial to identify them as instances of the experimental conditions
-trial_type={'pumps_demean'};
+trial_type={' '};
 
 %OPTIONAL. Response time measured in seconds. A negative response time can be used to represent preemptive responses and “n/a” denotes a missed response.
-response_time=[2.420]';
+response_time=[0]';
 
 %OPTIONAL Represents the location of the stimulus file (image, video, sound etc.) presented at the given onset time
-stim_file={''}; 
+stim_file={' '}; 
 
 %OPTIONAL Hierarchical Event Descriptor (HED) Tag.
-HED= {''}; 
+HED= {' '}; 
 
 t = table(onset,duration,trial_type,response_time,stim_file,HED);
 
