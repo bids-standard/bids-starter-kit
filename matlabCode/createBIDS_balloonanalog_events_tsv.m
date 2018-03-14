302c4aa3e76f118daef54b9293c6d4f6e5e4b209
Anushka Bhandari <anushka16134@iiitd.ac.in>
2018-03-14 18:01:27 +0530

    Update createBIDS_balloonanalog_events_tsv.m
    I have made the changes to align the Template with the specifications. This now contains a set of REQUIRED and OPTIONAL columns

--- matlabCode/createBIDS_balloonanalog_events_tsv.m
+++ matlabCode/createBIDS_balloonanalog_events_tsv.m
@@ -10,7 +10,7 @@ root_dir = '../';
 project_label = 'templates';
 sub_id = '01';
 ses_id = '01';
-task_id = 'balloonanalogrisktask';
+task_id = 'balloonanalogrisktask'; %example task 'balloonanalogrisktask'
 
 acquisition = 'func';
 run_id='01';
@@ -23,18 +23,27 @@ events_tsv_name = fullfile(root_dir,project_label,...
               '_ses-' ses_id ...
               '_task-' task_id ...
               '_run-' run_id '_events.tsv']);
-%% make an _events table and save 
-onset = [0.061]'; % onsets in seconds
-duration = [0.772]'; % duration in seconds (measured from onset)
-trial_type={'pumps_demean'};  %Primary categorisation of each trial to identify them as instances of the experimental conditions
-cash_demean={'n/a'}; %
-control_pumps_demean={'n/a'};
-explode_demean={'n/a'};
-pumps_demean= [-2.00]';
-response_time=[2.420]'; %Response time measured in seconds. A negative response time can be used to represent preemptive responses and “n/a” denotes a missed response.
+%% make a _events table and save 
+%% CONTAINS a set of REQUIRED and OPTIONAL columns
+onset = [0.061]'; %REQUIRED Onset (in seconds) of the event  measured from the beginning of the acquisition of the first volume in the corresponding task imaging data file.  If any acquired scans have been discarded before forming the imaging data file, ensure that a time of 0 corresponds to the first image stored. In other words negative numbers in “onset” are allowed.
 
+% REQUIRED. Duration of the event (measured  from onset) in seconds.  Must always be either zero or positive. A "duration" value of zero implies that the delta function or event is so short as to be effectively modeled as an impulse.
+duration = [0.772]'; 
 
-t = table(onset,duration,trial_type,cash_demean,control_pumps_demean,explode_demean,pumps_demean,response_time);
+%OPTIONAL Primary categorisation of each trial to identify them as instances of the experimental conditions
+trial_type={'pumps_demean'};
+
+%OPTIONAL. Response time measured in seconds. A negative response time can be used to represent preemptive responses and “n/a” denotes a missed response.
+response_time=[2.420]';
+
+%OPTIONAL Represents the location of the stimulus file (image, video, sound etc.) presented at the given onset time
+stim_file={''}; 
+
+%OPTIONAL Hierarchical Event Descriptor (HED) Tag.
+HED= {''}; 
+
+t = table(onset,duration,trial_type,response_time,stim_file,HED);
 
 writetable(t,events_tsv_name,'FileType','text','Delimiter','\t');
+
 
