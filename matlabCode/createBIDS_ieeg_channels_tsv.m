6dc0e0fe7f65ecd963da5748201014c662d9fe42
Remi Gau <remi_gau@hotmail.com>
2018-09-10 13:41:25 +0200

    clean comments

--- matlabCode/createBIDS_ieeg_channels_tsv.m
+++ matlabCode/createBIDS_ieeg_channels_tsv.m
@@ -1,11 +1,12 @@
 %% Template Matlab script to create an BIDS compatible electrodes.tsv file
 % This example lists all required and optional fields.
-% When adding additional metadata please use camelcase
+% When adding additional metadata please use CamelCase 
 %
 % DHermes, 2017
+% modified RG 201809
 
 %%
-clear all
+clear
 root_dir = '../';
 project_label = 'templates';
 ieeg_sub = '01';
@@ -21,31 +22,55 @@ channels_tsv_name = fullfile(root_dir,project_label,...
     '_run-' ieeg_run '_channels.tsv']);
 
 
+
 %% make a participants table and save 
 
-% required columns
-name = {''}; % Label of the channel, only contains letters and numbers. The label must correspond to _electrodes.tsv name and all ieeg type channels are required to have a position. In case of bipolar recordings where the channels are plugged into the hardware in bipolar manner, the channel name must be ?Name01-Name02? with a dash between the two electrode names.  (required).
-type = {''}; % Type of channel, see below for adequate keywords in this field (required), e.g. SEEG, ECOG. 
-units = {''}; % Physical unit of the value represented in this channel, e.g. V for Volt, specified according to the SI unit symbol and possibly prefix (e.g. milliV, microV), see BIDS spec for Units and Prefixes  (required).
-sampling_frequency = [0];% Sampling rate of the channel in Hz (required).
-low_cutoff = [0]; %Frequencies used for the low pass filter applied to the channel in Hz. If no low pass filter was applied, use n/a. Note that anti-alias is a low pass filter, specify its frequencies here if applicable (required).
-high_cutoff = [0]; % Frequencies used for the high pass filter applied to the channel in Hz. If no high pass filter applied, use n/a (required).
-notch = [0]; % Frequencies used for the notch filter applied to the channel, in Hz. If no notch filter applied, use n/a (required). 
 
-% recommended columns:
+
+%% required columns
+name = {''}; % Label of the channel, only contains letters and numbers. 
+% The label must correspond to _electrodes.tsv name and all ieeg type channels 
+% are required to have a position. In case of bipolar recordings where the 
+% channels are plugged into the hardware in bipolar manner, the channel name 
+% must be ?Name01-Name02? with a dash between the two electrode names.
+
+type = {''}; % Type of channel, see below for adequate keywords in this field 
+% e.g. SEEG, ECOG. 
+
+units = {''}; % Physical unit of the value represented in this channel,
+% e.g. V for Volt, specified according to the SI unit symbol and possibly 
+% prefix (e.g. milliV, microV), see BIDS spec for Units and Prefixes.
+
+sampling_frequency = [0];% Sampling rate of the channel in Hz.
+
+low_cutoff = [0]; %Frequencies used for the low pass filter applied to the 
+% channel in Hz. If no low pass filter was applied, use n/a. Note that 
+% anti-alias is a low pass filter, specify its frequencies here if applicable.
+
+high_cutoff = [0]; % Frequencies used for the high pass filter applied to 
+% the channel in Hz. If no high pass filter applied, use n/a.
+
+notch = [0]; % Frequencies used for the notch filter applied to the channel, 
+% in Hz. If no notch filter applied, use n/a (required). 
+
+
+
+%% recommended columns:
 group = {''}; % Which group of channels (grid/strip/probe) this channel belongs to. One group has one wire and noise can be shared. This can be a name or number (recommended).
 reference = {''}; % Specification of the reference (options: ?bipolar?, ?mastoid?, ?intracranial?, ?ElectrodeName01?) (recommended).
 
-% optional  columns
+
+
+%% optional  columns
 description = {''}; % Brief free-text description of the channel, or other information of interest (e.g. position (e.g., ?left lateral temporal surface?, ?unipolar/bipolar?, etc.). 
 status = {''}; %: Has good or bad: good channels can be taken into analysis. Bad indicates that the channel does not measure physiology: it is excessively noisy or broken or sitting on top of another grid, throughout the whole recording. More subtle descriptions should go in status_description.
 status_description = {''}; % Freeform text to specify subtle noise, or why a channel is bad.
 software_filters = {''}; % Optional field in case additional software filters were used.
 
+
+
+%% write
 t = table(name,type,units,sampling_frequency,low_cutoff,high_cutoff,...
     notch,group,reference,description,status,status_description,software_filters);
 
 writetable(t,channels_tsv_name,'FileType','text','Delimiter','\t');
-
-clear size
-
--- matlabCode/createBIDS_ieeg_electrodes_tsv.m
+++ matlabCode/createBIDS_ieeg_electrodes_tsv.m
@@ -1,11 +1,13 @@
 %% Template Matlab script to create an BIDS compatible electrodes.tsv file
 % This example lists all required and optional fields.
-% When adding additional metadata please use camelcase
+% When adding additional metadata please use CamelCase 
 %
 % DHermes, 2017
+% modified RG 201809
+
 
 %%
-clear all
+clear
 root_dir = '../';
 project_label = 'templates';
 ieeg_sub = '01';
@@ -18,9 +20,13 @@ electrodes_tsv_name = fullfile(root_dir,project_label,...
     '_electrodes.tsv']);
 
 
+
 %% make a participants table and save 
 
-% required columns
+
+
+%% required columns
+
 name = {''}; % Name of the electrode
 x = [0]';
 y = [0]';
@@ -28,15 +34,24 @@ z = [0]';
 size = [0]'; % Diameter in mm 
 type = [0]'; % Type of intracranial electrode, one of [?surface?,  ?depth? , ?dbs?] 
 
-% optional columns:
-material = {''}; % Material of the electrodes
-tissue = {''}; % If a clinician has made an observation about the tissue underneath the electrode  (e.g. epilepsy, tumor, if nothing state n/a)
-manufacturer = {''}; % Optional field to specify the electrode manufacturer for each electrode. Can be used if electrodes were manufactured by more than one company.
-grid_size = {''}; % Optional field to specify the dimensions of the grid the electrode is a part of. Should be of the form `(M, N)`. E.g., (8, 8) or(1, 4).
 
+
+%% optional columns
+
+material = {''}; % Material of the electrodes
+
+tissue = {''}; % If a clinician has made an observation about the tissue 
+% underneath the electrode  (e.g. epilepsy, tumor, if nothing state n/a)
+
+manufacturer = {''}; % Optional field to specify the electrode manufacturer 
+% for each electrode. Can be used if electrodes were manufactured by more than one company.
+
+grid_size = {''}; % Optional field to specify the dimensions of the grid the 
+% electrode is a part of. Should be of the form `(M, N)`. E.g., (8, 8) or(1, 4).
+
+
+
+%% write
 t = table(name,x,y,z,size,type,material,tissue,manufacturer,grid_size);
 
 writetable(t,electrodes_tsv_name,'FileType','text','Delimiter','\t');
-
-clear size
-
