b46c5f048fb563c5ddb032f43c390dffdd1bf134
Elizabeth DuPre <emd222@cornell.edu>
2018-10-26 08:09:08 -0400

    Linting, update line lengths

--- pythonCode/create_MP2RAGE_json.py
+++ pythonCode/create_MP2RAGE_json.py
@@ -18,23 +18,24 @@ import json
 import glob
 from collections import OrderedDict
 
-
-
 # DEFINE CONTENT OF JSON FILES
 
-# defining the content of the JSON file for the first inversion image (sub-*_inv-1_MPRAGE.json)
+# defining the content of the JSON file for the
+# first inversion image (sub-*_inv-1_MPRAGE.json)
 data_inv_1 = OrderedDict([
     ('InversionTime', '900'),
     ('FlipAngle', '5')  # ms
 ])
 
-# defining the content of the JSON file for the second inversion image (sub-*_inv-2_MPRAGE.json)
+# defining the content of the JSON file for the
+# second inversion image (sub-*_inv-2_MPRAGE.json)
 data_inv_2 = OrderedDict([
     ('InversionTime', '2750'),
     ('FlipAngle', '3')  # ms
 ])
 
-# defining the content of the JSON files for the T1w (sub-*_T1w.json) and the T1map (sub-*_T1map.json)
+# defining the content of the JSON files for the
+# T1w (sub-*_T1w.json) and the T1map (sub-*_T1map.json)
 data_T1 = OrderedDict([
     ('EstimationMethod', 'Marques et al., 2013'),
 ])
@@ -88,62 +89,59 @@ data_MP2RAGE = OrderedDict([
 
 indent = 4
 
-start_dir = "D:\\BIDS\\7t_mp2rage"  # insert here path to your BIDS data set
+start_dir = ''  # insert here path to your BIDS data set
 
-# list all subjects
-subj_ls = glob.iglob(os.path.join(start_dir, 'sub*'))  # create iterator with fullpath for subjects folder
+# list all subjects and  create iterator with full path for subjects folder
+subj_ls = glob.iglob(os.path.join(start_dir, 'sub*'))
 
 for iSubj in subj_ls:
-
-    print(iSubj)
 
     # list all subfolders and files for that subject
     subj_dir = os.walk(iSubj)
 
     # go through all the files for that subject
     for path, subdirs, files in subj_dir:
-        for name in files:
-            # creates the json files in the folder where the magnitude image of the first inversion is found
-            if '_inv-1_part-mag_MPRAGE.nii.gz' in name:
-                print(os.path.join(path, name))
+        for f in files:
+            # creates the json files in the folder where the magnitude image
+            # of the first inversion is found
+            if '_inv-1_part-mag_MPRAGE.nii.gz' in f:
 
-                basename = '_'.join(name.split('_')[:-3])  # excludes last three key-value pairs
+                #  define filename, excluding last three key-value pairs
+                fname = '_'.join(f.split('_')[:-3])
 
                 json_folder = path
 
                 # creating JSON file for the first inversion image
-                json_name = basename + '_inv-1_MPRAGE.json'
+                json_name = '{}_inv-1_MPRAGE.json'.format(fname)
                 # create the file
                 with open(os.path.join(json_folder, json_name), 'w') as ff:
                     json.dump(data_inv_1, ff, sort_keys=False, indent=indent)
 
                 # creating JSON file for the second inversion image
-                json_name = basename + '_inv-2_MPRAGE.json'
+                json_name = '{}_inv-2_MPRAGE.json'.format(fname)
                 with open(os.path.join(json_folder, json_name), 'w') as ff:
                     json.dump(data_inv_2, ff, sort_keys=False, indent=indent)
 
                 # creating main JSON file for the MP2RAGE
-                json_name = basename + '_MPRAGE.json'
+                json_name = '{}_MPRAGE.json'.format(fname)
                 with open(os.path.join(json_folder, json_name), 'w') as ff:
                     json.dump(data_MP2RAGE, ff, sort_keys=False, indent=indent)
 
-                # adding content to JSON files for the T1w and T1map as its content is subject dependent
-                name_inv1_mag_img = os.path.join('anat', basename + '_inv1_part-mag_MPRAGE.nii.gz')
-                name_inv1_phs_img = os.path.join('anat', basename + '_inv1_part-phase_MPRAGE.nii.gz')
-                name_inv2_mag_img = os.path.join('anat', basename + '_inv1_part-mag_MPRAGE.nii.gz')
-                name_inv2_phs_img = os.path.join('anat', basename + '_inv1_part-phase_MPRAGE.nii.gz')
+                # adding content to JSON files for the T1w and T1map
+                # as its content is subject dependent
+                inv1_mag = os.path.join('anat', '{}_inv1_part-mag_MPRAGE.nii.gz'.format(fname))
+                inv1_phs = os.path.join('anat', '{}_inv1_part-phase_MPRAGE.nii.gz'.format(fname))
+                inv2_mag = os.path.join('anat', '{}_inv1_part-mag_MPRAGE.nii.gz'.format(fname))
+                inv2_phs = os.path.join('anat', '{}_inv1_part-phase_MPRAGE.nii.gz'.format(fname))
 
-                data_T1['BasedOn'] = \
-                    name_inv1_mag_img + ', ' + \
-                    name_inv1_phs_img + ', ' + \
-                    name_inv2_mag_img + ', ' + \
-                    name_inv2_phs_img + ', '
+                data_T1['BasedOn'] = '{0}, {1}, {2}, {3}, '.format(inv1_mag, inv1_phs,
+                                                                   inv2_mag, inv2_phs)
 
                 # creating JSON files for the T1w and T1map
-                json_name = basename + '_T1map.json'
+                json_name = fname + '_T1map.json'
                 with open(os.path.join(json_folder, json_name), 'w') as ff:
                     json.dump(data_T1, ff, sort_keys=False, indent=indent)
 
-                json_name = basename + '_T1w.json'
+                json_name = fname + '_T1w.json'
                 with open(os.path.join(json_folder, json_name), 'w') as ff:
                     json.dump(data_T1, ff, sort_keys=False, indent=indent)
